6bec23664b21d603cd72cc6fa006cb261d203be2
Michael Harris <jmichaeliii@yahoo.com>
2017-04-10 17:21:59 -0400

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCReviewSite.Models
+{
+    public class Category
+    {
+        // Properties
+        [Key]
+        public int ID { get; set; }
+        public string TechName { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCReviewSite.Models
+{
+    public class Review
+    {
+        // Properties
+        [Key]
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public int UserRating { get; set; }
+        public string Description { get; set; }
+
+        // Navigation Properties
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
